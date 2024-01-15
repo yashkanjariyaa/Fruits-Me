deb359917eb83ad20d00509248238cf936f0d7f8
yashkanjariyaa <yashkanjariyaff@gmail.com>
2024-01-16 00:58:18 +0530

    added env commit

--- server/database/connection.js
+++ server/database/connection.js
@@ -1,9 +1,11 @@
 const mysql = require("mysql2/promise");
-
+const dotenv = require("dotenv");
+dotenv.config();
+const password = process.env.dbpassword;
 const pool =  mysql.createPool({
     host: "localhost",
     user: "root",
-    password: "yash1311",
+    password: password,
     database: "shoppingdb",
     waitForConnections: true,
     connectionLimit: 10,
